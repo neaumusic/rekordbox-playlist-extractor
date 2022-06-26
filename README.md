9e2b96a91f170b56e35269467c1ee955897d9237
neaumusic <jsw.james.wilson@gmail.com>
2022-06-25 17:48:35 -0700

    feat: init

---
+++ README.md
@@ -0,0 +1,27 @@
+Rekordbox Playlist Extractor
+--
+
+Parses playlists from rekordbox.xml into folders of .m3u8 files
+
+Useful for transferring tagged tracks / updated intelligent playlist tracks back into Apple Music
+
+---
+
+Usage
+--
+
+You need to export a `"rekordbox.xml"` file into this directory and run `node index.js`
+
+
+Prerequisites
+--
+You initially need to run `yarn` to install dependencies
+
+If you don't have `yarn` you can install it via [Homebrew](brew.sh) which is where Apple was founded
+
+eg `brew install yarn`
+
+You should have Node (which executes JS with the same V8 runtime as Chrome but outside of a browser),
+but if you don't, you can probably install it with `brew install node` as well.
+
+On Windows, I'm not sure how you can install yarn/node but it should be fairly simple
---
+++ index.js
@@ -0,0 +1,68 @@
+import fs from "fs";
+import { DOMParser } from "xmldom";
+import xpath from "xpath";
+
+const rbxml = fs.readFileSync("./rekordbox.xml", "utf8");
+const doc = new DOMParser().parseFromString(rbxml);
+
+const tracksCache = Array.from(xpath.select("/DJ_PLAYLISTS/COLLECTION/TRACK", doc)).reduce((cache, t) => {
+  cache[t.getAttribute("TrackID")] = t;
+  return cache;
+}, {});
+
+const root = xpath.select("/DJ_PLAYLISTS/PLAYLISTS/NODE[@Name='ROOT']", doc)[0];
+const folders = Array.from(xpath.select("./NODE[@Type='0']", root));
+const playlists = Array.from(xpath.select("./NODE[@Type='1']", root));
+const outputDirectory = "playlists";
+const folderDelimiter = "#";
+
+fs.rmSync(outputDirectory, { recursive: true, force: true });
+fs.mkdirSync(outputDirectory);
+
+playlists.forEach(p => {
+  parsePlaylist(p, "");
+});
+
+folders.forEach(f => {
+  parseFolder(f, "");
+});
+
+function parseFolder (folder, pathPrefix) {
+  const folderName = folder.getAttribute("Name").replace(/[/\\?%*:|"<>]/g, '-');
+  const path = `${pathPrefix}${folderName}`;
+
+  const folders = Array.from(xpath.select("./NODE[@Type='0']", folder));
+  const playlists = Array.from(xpath.select("./NODE[@Type='1']", folder));
+
+  console.log(`-----------> parsing ${path}`);
+  playlists.forEach(p => {
+    parsePlaylist(p, path);
+  });
+
+  folders.forEach(f => {
+    parseFolder(f, `${path}${folderDelimiter}`);
+  });
+}
+
+function parsePlaylist (playlist, pathPrefix) {
+  const playlistName = playlist.getAttribute("Name").replace(/[/\\?%*:|"<>]/g, '-');
+  const path = `${pathPrefix}${folderDelimiter}${playlistName}.m3u8`;
+
+  const trackIds = Array.from(xpath.select("./TRACK", playlist)).map(t => t.getAttribute("Key"));
+  const tracks = trackIds.map(id => tracksCache[id]);
+
+  console.log(`- ${outputDirectory}/${path}`);
+  fs.writeFileSync(`${outputDirectory}/${path}`, [
+    "#EXTM3U",
+    tracks.map(t => parseTrack(t))
+  ].join('\n'));
+}
+
+function parseTrack (track) {
+  const totalTime = track.getAttribute("TotalTime");
+  const artist = track.getAttribute("Artist");
+  const trackName = track.getAttribute("Name");
+  const location = decodeURIComponent(track.getAttribute("Location"));
+
+  return `#EXTINF:${totalTime},${artist} - ${trackName}\n${location}\n`;
+}
